efbee7e86795c16235b2b123347c04c1771eca26
SADAHIRO Tomoyuki <SADAHIRO@cpan.org>
2011-07-31 10:48:50 +0000

    Import of SADAHIRO/Lingua-FA-MacFarsi-0.20 from CPAN.
    gitpan-cpan-distribution: Lingua-FA-MacFarsi
    gitpan-cpan-version:      0.20
    gitpan-cpan-path:         SADAHIRO/Lingua-FA-MacFarsi-0.20.tar.gz
    gitpan-cpan-author:       SADAHIRO
    gitpan-cpan-maturity:     released

--- MacFarsi.pm
+++ MacFarsi.pm
@@ -7,7 +7,7 @@ use strict;
 require Exporter;
 require DynaLoader;
 
-our $VERSION = '0.10';
+our $VERSION = '0.20';
 our @ISA = qw(Exporter DynaLoader);
 our @EXPORT = qw(decodeMacFarsi encodeMacFarsi);
 our @EXPORT_OK = qw(decode encode);
